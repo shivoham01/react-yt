6576a0c2a4a62cc1ce071b63c4e165750ac1b375
shivoham01 <theashutoshshukl@gmail.com>
2026-01-03 19:10:41 +0530

    card-project-update

--- Card-Project/src/components/Card.jsx
+++ Card-Project/src/components/Card.jsx
@@ -1,28 +1,28 @@
-import reactSVG from "../assets/react.svg"
 import "../App.css"
 import { Bookmark } from "lucide-react"
 
-const Card = () => {
+
+const Card = (props) => {
     return (
             <div className="card">
                 <div>
                     <div className="top">
-                        <img src={reactSVG} alt="logo" />
+                        <img src={props.brandLogo} alt="logo" />
                         <button>Save <Bookmark size={15} /></button>
                     </div>
                     <div className="center">
-                        <h3>Google <span>30 days ago</span></h3>
-                        <h2>Senior UI/UX Designer</h2>
+                        <h3>{props.companyName} <span>{props.datePosted}</span></h3>
+                        <h2>{props.post}</h2>
                         <div className="tags">
-                            <div className="tag">Part-time</div>
-                            <div className="tag">Senior level</div>
+                            <div className="tag">{props.tag1}</div>
+                            <div className="tag">{props.tag2}</div>
                         </div>
                     </div>
                 </div>
                 <div className="bottom">
                     <div>
-                        <h3>$120/hr</h3>
-                        <span>San Francisco, CA</span>
+                        <h3>{props.pay}</h3>
+                        <span>{props.location}</span>
                     </div>
                     <button>Apply now</button>
                 </div>
